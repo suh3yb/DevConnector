39a2fc9c4097f8a2442d00a8593cec56105f66a1
shybc <suheyb.c@gmail.com>
2019-09-16 13:15:46 +0200

    added MongoDB server connection

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
