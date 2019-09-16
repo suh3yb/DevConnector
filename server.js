37a582b6e6d679487770fdf7ad47923be16595d7
shybc <suheyb.c@gmail.com>
2019-09-16 14:19:13 +0200

    added json middleware for req.body, created user registration model for validation and added express-validator to users route with some checks

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
