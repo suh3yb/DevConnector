444f5c5c3884c2cd2cfec7fa5f4fb07c8d21c817
shybc <suheyb.c@gmail.com>
2019-09-23 15:50:08 +0200

    added logic for user login, added redirect if logged in

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,11 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from '../actions/types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -19,6 +26,7 @@ export default function(state = initialState, action) {
         user: payload,
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -28,12 +36,14 @@ export default function(state = initialState, action) {
       };
     case AUTH_ERROR:
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
         token: null,
         isAuthenticated: false,
         loading: false,
+        // should have set user to null !!!
       };
     default:
       return state;
