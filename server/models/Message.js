a36fa07d1a8caac10ef9463c5fd2f9a7bc09758f
heartwhite <abdullahakyurek@gmail.com>
2019-10-17 13:40:00 +0200

    changes copied from hamza s branch manually in files User and Message models, Getcurrentprofile and getuser routes in apis

--- server/models/Message.js
+++ server/models/Message.js
@@ -3,23 +3,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
-  text: {
-    type: String,
-    max: 2000,
-  },
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
+  conversation: [
+    {
+      text: {
+        type: String,
+        max: 2000,
+      },
+      sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
 
-  receiver: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+      receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
 });
 
 const Message = mongoose.model('message', MessageSchema);
