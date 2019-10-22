3fc687004454a076205ed7d4245f9e2f1a2109b5
zehrakopar <zehrakopar@yahoo.com.tr>
2019-10-22 15:50:41 +0200

    'backend'

--- server/routes/api/posts/likePost.js
+++ server/routes/api/posts/likePost.js
@@ -1,34 +1,30 @@
 'use strict';
 
 const Post = require('../../../models/Post');
+const User = require('../../../models/User');
 
 const likePost = async (req, res) => {
   try {
+    const user = await User.findById(req.user.id).select('-password');
     const post = await Post.findById(req.params.id);
-
-    if (!post) {
-      return res.status(404).json({ msg: 'Post not found' });
-    }
-
-    // Check if the post is already be liked
-    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
-      return res.status(400).json({ msg: 'Post already liked' });
-    }
-
-    post.likes.unshift({ user: req.user.id });
-
-    await post.save();
-
-    res.json(post.likes);
+        // Check if the post has already been liked
+        if (post.likes.filter((like) => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Post already liked' });
+        }
+    const newLike = {      
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    };
+        post.likes.unshift(newLike);
+        await post.save();
+        res.json(post.likes);
   } catch (error) {
     console.error(error.message);
-
     if (error.kind === 'ObjectId') {
       return res.status(404).json({ msg: 'Post not found' });
     }
-
     res.send('Server Error');
   }
 };
-
 module.exports = likePost;
