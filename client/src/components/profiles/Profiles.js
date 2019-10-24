93e7ff3b333a00084042cf59799a65a0e2131391
Ghufran Deiri <ghifoo85@gmail.com>
2019-10-24 15:59:16 +0200

    search bar frontend

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -3,48 +3,52 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileItem from './ProfileItem';
+import Search from './Search';
 import { getCurrentProfile, getProfiles } from '../../redux/actions/profileAction';
 
 const Profiles = ({ getCurrentProfile, getProfiles, profile: { profiles, loading } }) => {
-  useEffect(() => {
-    getProfiles();
-    getCurrentProfile();
-  }, [getProfiles, getCurrentProfile]);
+	useEffect(
+		() => {
+			getProfiles();
+			getCurrentProfile();
+		},
+		[ getProfiles, getCurrentProfile ],
+	);
 
-  return (
-    <Fragment>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Fragment>
-          <h1 className="large text-primary">Developers</h1>
-          <p className="lead">
-            <i className="fab fa-connectdevelop"></i> Browse and connect with developers
-          </p>
-          <div className="profiles">
-            {profiles.length > 0 ? (
-              profiles.map(profile => <ProfileItem key={profile._id} profile={profile} />)
-            ) : (
-              <h4>No profile found...</h4>
-            )}
-          </div>
-        </Fragment>
-      )}
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			{loading ? (
+				<Spinner />
+			) : (
+				<Fragment>
+					<Search />
+					<h1 className='large text-primary'>Developers</h1>
+					<p className='lead'>
+						<i className='fab fa-connectdevelop' /> Browse and connect with developers
+					</p>
+					<div className='profiles'>
+						{profiles.length > 0 ? (
+							profiles.map((profile) => (
+								<ProfileItem key={profile._id} profile={profile} />
+							))
+						) : (
+							<h4>No profile found...</h4>
+						)}
+					</div>
+				</Fragment>
+			)}
+		</Fragment>
+	);
 };
 
 Profiles.propTypes = {
-  getProfiles: PropTypes.func.isRequired,
-  getCurrentProfile: PropTypes.func.isRequired,
-  profile: PropTypes.object.isRequired,
+	getProfiles: PropTypes.func.isRequired,
+	getCurrentProfile: PropTypes.func.isRequired,
+	profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-  profile: state.profile,
+const mapStateToProps = (state) => ({
+	profile: state.profile,
 });
 
-export default connect(
-  mapStateToProps,
-  { getCurrentProfile, getProfiles },
-)(Profiles);
+export default connect(mapStateToProps, { getCurrentProfile, getProfiles })(Profiles);
