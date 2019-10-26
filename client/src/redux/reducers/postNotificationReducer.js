7782229d01df2aa19ffe138954d9e678088aa049
lizozo <hamza.erguc@gmail.com>
2019-10-26 22:18:56 +0300

    removed unnecessary reducer types

--- client/src/redux/reducers/postNotificationReducer.js
+++ client/src/redux/reducers/postNotificationReducer.js
@@ -1,18 +1,13 @@
 import { GET_UPDATE } from '../actions/types';
-import { RESET_UPDATE, POST_ERROR } from '../actions/types';
+import { RESET_UPDATE } from '../actions/types';
 const initialState = { length: 0, error: {} };
 export default function(state = initialState, action) {
-  const { type, payload } = action;
+  const { type } = action;
   switch (type) {
     case GET_UPDATE:
       return { ...state, length: ++state.length };
     case RESET_UPDATE:
       return { ...state, length: 0 };
-    case POST_ERROR:
-      return {
-        ...state,
-        error: payload,
-      };
     default:
       return state;
   }
