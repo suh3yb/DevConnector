b1bbdbcb34913e599b49df6135d30343873a396d
Ghufran Deiri <ghifoo85@gmail.com>
2019-10-26 12:08:52 +0200

    renaming

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -8,81 +8,86 @@ import { searchProfile } from '../../redux/actions/profileAction';
 import { getCurrentProfile, getProfiles } from '../../redux/actions/profileAction';
 
 const Profiles = ({
-  getCurrentProfile,
-  getProfiles,
-  profile: { profiles, loading, search },
-  searchProfile,
+	getCurrentProfile,
+	getProfiles,
+	profile: { profiles, loading, search },
+	searchProfile,
 }) => {
-  useEffect(() => {
-    getProfiles();
-    getCurrentProfile();
-  }, [getProfiles, getCurrentProfile]);
+	useEffect(
+		() => {
+			getProfiles();
+			getCurrentProfile();
+		},
+		[ getProfiles, getCurrentProfile ],
+	);
 
-  const searchResult = profiles => {
-    const results = profiles.map(
-      profile =>
-        profile.user.name.toUpperCase().includes(search.name.toUpperCase()) && (
-          <ProfileItem key={profile._id} profile={profile} />
-        ),
-    );
+	const searchResult = (profiles) => {
+		const results = profiles.map(
+			(profile) =>
+				profile.user.name.toLowerCase().includes(search.name.toLowerCase()) && (
+					<ProfileItem key={profile._id} profile={profile} />
+				),
+		);
 
-    return results.filter(item => typeof item === 'object').length ? (
-      results
-    ) : (
-      <p>No profiles found...</p>
-    );
-  };
+		return results.filter((item) => typeof item === 'object').length ? (
+			results
+		) : (
+			<p>No profiles found...</p>
+		);
+	};
 
-  return (
-    <Fragment>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Fragment>
-          <h1 className="large text-primary">Developers</h1>
-          <div className="search-bar-container">
-            <p className="lead">
-              <i className="fab fa-connectdevelop" /> Browse and connect with developers
-            </p>
-            <Search profiles={profiles} />
-          </div>
-          {search && (
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={() => {
-                searchProfile(null);
-              }}
-            >
-              Back To Profiles
-            </button>
-          )}
-          <div className="profiles">
-            {profiles.length > 0 && !search ? (
-              profiles.map(profile => <ProfileItem key={profile._id} profile={profile} />)
-            ) : profiles.length > 0 && search ? (
-              searchResult(profiles)
-            ) : (
-              <h4>No profiles found...</h4>
-            )}
-          </div>
-        </Fragment>
-      )}
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			{loading ? (
+				<Spinner />
+			) : (
+				<Fragment>
+					<h1 className='large text-primary'>Developers</h1>
+					<div className='search-bar-container'>
+						<p className='lead'>
+							<i className='fab fa-connectdevelop' /> Browse and connect with
+							developers
+						</p>
+						<Search profiles={profiles} />
+					</div>
+					{search && (
+						<button
+							type='button'
+							className='btn btn-primary'
+							onClick={() => {
+								searchProfile(null);
+							}}
+						>
+							Back To Profiles
+						</button>
+					)}
+					<div className='profiles'>
+						{profiles.length > 0 && !search ? (
+							profiles.map((profile) => (
+								<ProfileItem key={profile._id} profile={profile} />
+							))
+						) : profiles.length > 0 && search ? (
+							searchResult(profiles)
+						) : (
+							<h4>No profiles found...</h4>
+						)}
+					</div>
+				</Fragment>
+			)}
+		</Fragment>
+	);
 };
 
 Profiles.propTypes = {
-  getProfiles: PropTypes.func.isRequired,
-  getCurrentProfile: PropTypes.func.isRequired,
-  profile: PropTypes.object.isRequired,
+	getProfiles: PropTypes.func.isRequired,
+	getCurrentProfile: PropTypes.func.isRequired,
+	profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-  profile: state.profile,
+const mapStateToProps = (state) => ({
+	profile: state.profile,
 });
 
-export default connect(
-  mapStateToProps,
-  { getCurrentProfile, getProfiles, searchProfile },
-)(Profiles);
+export default connect(mapStateToProps, { getCurrentProfile, getProfiles, searchProfile })(
+	Profiles,
+);
--- client/src/components/profiles/Search.js
+++ client/src/components/profiles/Search.js
@@ -5,51 +5,52 @@ import { connect } from 'react-redux';
 import { searchProfile } from '../../redux/actions/profileAction';
 
 const Search = ({ searchProfile, profiles }) => {
-  const [input, setInput] = useState('');
+	const [ input, setInput ] = useState('');
 
-  const names = profiles.map(profile => ({ name: profile.user.name, id: profile.user._id }));
-  const suggestions =
-    input && names.filter(user => user.name.toLowerCase().includes(input.toLowerCase()));
+	const users = profiles.map((profile) => ({ name: profile.user.name, id: profile.user._id }));
+	const suggestions =
+		input && users.filter((user) => user.name.toLowerCase().includes(input.toLowerCase()));
 
-  return (
-    <Fragment>
-      <div className="search-container">
-        <form
-          className="search-label"
-          onSubmit={e => {
-            e.preventDefault();
-            searchProfile({ name: input });
-            setInput('');
-          }}
-        >
-          <input
-            type="text"
-            placeholder=" Search..."
-            value={input}
-            onChange={e => setInput(e.target.value)}
-          />
-          <i className="fa fa-search search-icon" />
-          {suggestions.length > 0 && (
-            <div className="bg-light live-search-container">
-              {suggestions.slice(0, 6).map(user => (
-                <Link to={`/profile/${user.id}`} style={{ display: 'block' }} key={user.id}>
-                  {user.name}
-                </Link>
-              ))}
-            </div>
-          )}
-        </form>
-      </div>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<div className='search-container'>
+				<form
+					className='search-label'
+					onSubmit={(e) => {
+						e.preventDefault();
+						searchProfile({ name: input });
+						setInput('');
+					}}
+				>
+					<input
+						type='text'
+						placeholder=' Search...'
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+					/>
+					<i className='fa fa-search search-icon' />
+					{suggestions.length > 0 && (
+						<div className='bg-light live-search-container'>
+							{suggestions.slice(0, 6).map((user) => (
+								<Link
+									to={`/profile/${user.id}`}
+									style={{ display: 'block' }}
+									key={user.id}
+								>
+									{user.name}
+								</Link>
+							))}
+						</div>
+					)}
+				</form>
+			</div>
+		</Fragment>
+	);
 };
 
 Search.propTypes = {
-  searchProfile: PropTypes.func.isRequired,
-  profiles: PropTypes.array.isRequired,
+	searchProfile: PropTypes.func.isRequired,
+	profiles: PropTypes.array.isRequired,
 };
 
-export default connect(
-  null,
-  { searchProfile },
-)(Search);
+export default connect(null, { searchProfile })(Search);
