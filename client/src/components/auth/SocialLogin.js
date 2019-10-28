96ff971e47d4305487c22f0e7cff853cc5551000
Suheyb Coskun <suheyb.c@gmail.com>
2019-10-28 09:43:06 +0100

    remove Twitter login

--- client/src/components/auth/SocialLogin.js
+++ client/src/components/auth/SocialLogin.js
@@ -23,7 +23,6 @@ const uiConfig = {
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
   ],
   callbacks: {
